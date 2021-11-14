976b8972e42322588aef43ad1e5de4b580122475
Vitali Dreko <vitalidreko@MacBook-Air-Vitali.local>
2021-11-15 01:04:46 +0300

    add additional profile info for /editing page

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -11,8 +11,7 @@ let ProfileInfo = (props) => {
         lifePosition: 'none',
         personalInformation: 'none'
     })
-
-    return (
+    return (  
         <>
             <div className={style.profile_aboutMe}>
                 <div className={`${style.separator} d-flex justify-content-between`}>
@@ -50,12 +49,12 @@ let ProfileInfo = (props) => {
                                 <NavLink to="/edit" style={{ display: editBtnVisability.main }} >Редактировать</NavLink>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>День Рождения:</div>
-                                <div className={style.profile_info_data}>8 июня</div>
+                                <div className={style.profile_info_item}>Родной город:</div>
+                                <div className={style.profile_info_data}>Минск</div>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>Город:</div>
-                                <div className={style.profile_info_data}>Минск</div>
+                                <div className={style.profile_info_item}>Языки:</div>
+                                <div className={style.profile_info_data}>Русский, Английский</div>
                             </div>
                         </div>
                         {/* Окончание основной информации */}
@@ -67,12 +66,24 @@ let ProfileInfo = (props) => {
                                 <NavLink to="/edit" style={{ display: editBtnVisability.contact }}>Редактировать</NavLink>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>День Рождения:</div>
-                                <div className={style.profile_info_data}>8 июня</div>
+                                <div className={style.profile_info_item}>Мобильный телефон:</div>
+                                <div className={style.profile_info_data}>+375(29)363-07-08</div>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>Город:</div>
-                                <div className={style.profile_info_data}>Минск</div>
+                                <div className={style.profile_info_item}>Доп. телефон:</div>
+                                <div className={style.profile_info_data}>+375(29)538-70-80</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Skype:</div>
+                                <div className={style.profile_info_data}>doka.by</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Twitter:</div>
+                                <div className={style.profile_info_data}>doka_by</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Личный сайт:</div>
+                                <div className={style.profile_info_data}>dreko.by</div>
                             </div>
                         </div>
                         {/* Окончание контактной информации */}
@@ -84,12 +95,28 @@ let ProfileInfo = (props) => {
                                 <NavLink to="/edit" style={{ display: editBtnVisability.lifePosition }}>Редактировать</NavLink>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>День Рождения:</div>
-                                <div className={style.profile_info_data}>8 июня</div>
+                                <div className={style.profile_info_item}>Полит. предпочтения:</div>
+                                <div className={style.profile_info_data}>Умеренные</div>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>Город:</div>
-                                <div className={style.profile_info_data}>Минск</div>
+                                <div className={style.profile_info_item}>Мировоззрение:</div>
+                                <div className={style.profile_info_data}>Атеист</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Главное в жизни:</div>
+                                <div className={style.profile_info_data}>Саморазвитие</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Главное в людях:</div>
+                                <div className={style.profile_info_data}>Ум и креативность</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Отношение к курению:</div>
+                                <div className={style.profile_info_data}>Компромисное</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Отношение к алкоголю:</div>
+                                <div className={style.profile_info_data}>Компромисное</div>
                             </div>
                         </div>
                         {/* Окончание жизненной позиции */}
@@ -101,12 +128,40 @@ let ProfileInfo = (props) => {
                                 <NavLink to="/edit" style={{ display: editBtnVisability.personalInformation }}>Редактировать</NavLink>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>День Рождения:</div>
-                                <div className={style.profile_info_data}>8 июня</div>
+                                <div className={style.profile_info_item}>Деятельность:</div>
+                                <div className={style.profile_info_data}>Программирование, бизнес</div>
                             </div>
                             <div className={style.profile_info_row}>
-                                <div className={style.profile_info_item}>Город:</div>
-                                <div className={style.profile_info_data}>Минск</div>
+                                <div className={style.profile_info_item}>Интересы:</div>
+                                <div className={style.profile_info_data}>Образование, развитие</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимая музыка:</div>
+                                <div className={style.profile_info_data}>Би-2, Pink Floyd</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимые фильмы:</div>
+                                <div className={style.profile_info_data}>Фильмы Гая Ричи</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимые телешоу:</div>
+                                <div className={style.profile_info_data}>Не смотрю телешоу</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимые книги:</div>
+                                <div className={style.profile_info_data}>Грокаем Алгоритмы</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимые игры:</div>
+                                <div className={style.profile_info_data}>Mu Online</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>Любимые цитаты:</div>
+                                <div className={style.profile_info_data}>Никогда не бойся делать то, что ты не умеешь.</div>
+                            </div>
+                            <div className={style.profile_info_row}>
+                                <div className={style.profile_info_item}>О себе:</div>
+                                <div className={style.profile_info_data}>Никогда не бойся делать то, что ты не умеешь.</div>
                             </div>
                         </div>
                         {/* Окончание личной информации */}
